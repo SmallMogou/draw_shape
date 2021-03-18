a26a6a79fee6c4a848051fe05920c6279162e7fa
littlemogou <aoielqj@126.com>
2021-03-18 12:54:26 +0800

    yq v1.0

--- src/Yq.java
+++ src/Yq.java
@@ -1,10 +1,15 @@
+/**
+    SE实践3
+    @author lm
+    since 1.0
+ */
 import java.io.*;
 
 class Yq{
     public static void main(String argv[]){
         try {
-            String filePathIn = "yq_in.txt";
-            String filePahtOut = "yq_out.txt";
+            String filePathIn = argv[0];
+            String filePathOut = argv[1];
             String encoding="GBK"; //编码格式
             File file=new File(filePathIn);
             if(file.isFile() && file.exists()){ //判断文件是否存在
@@ -13,18 +18,27 @@ class Yq{
                 BufferedReader bufferedReader = new BufferedReader(read);
                 
                 String lineTxt = null;
-                FileWriter fwriter = new FileWriter(filePahtOut, true);
+                FileWriter fwriter = new FileWriter(filePathOut, false);
                 String temp = "";
                 // 读取文件
                 while((lineTxt = bufferedReader.readLine()) != null){
-                     if(!temp.equals(lineTxt.substring(0, 3))){
+                    if(argv.length == 3){
+                        if(!argv[2].equals(lineTxt.substring(0, 3))) 
+                            continue;
+                        else if(!temp.equals(lineTxt.substring(0, 3))){
+                            temp = lineTxt.substring(0, 3);
+                            // 写入文件
+                            fwriter.write(temp + "\r\n");
+                        }
+                    }
+                    else if(!temp.equals(lineTxt.substring(0, 3))){
                         temp = lineTxt.substring(0, 3);
                         // 写入文件
-                        fwriter.write(temp+"\r\n");
+                        fwriter.write(temp + "\r\n");
                     }
                     if(!lineTxt.substring(4, lineTxt.length()).equals("待明确地区	0")){
                         // 写入文件
-                        fwriter.write(lineTxt.substring(4, lineTxt.length())+"\r\n");
+                        fwriter.write(lineTxt.substring(4, lineTxt.length()) + "\r\n");
                     }
                 }
                 read.close();
